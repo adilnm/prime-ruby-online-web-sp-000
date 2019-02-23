c7ff064b30ad7c74fbea05b410339976767a908f
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-23 06:23:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 # Add  code here!
 def prime?(integer)
+  
+  if(intger<=1)
+    return false
+  else if (integer==3)
+    return true
+  else 
 array=(2..(integer-1)).to_a
 if (array.all?{|i| integer%i==0})
   return true
